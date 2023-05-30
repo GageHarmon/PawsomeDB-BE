45e4da7bc752a2a994f8181054107244ee0b5225
Gage Harmon <gaharmon1@gmail.com>
2023-05-30 15:28:44 -0600

    fixed db init

--- Server/app.py
+++ Server/app.py
@@ -2,10 +2,9 @@ from flask import request, jsonify, session, make_response
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import User, Pet
 from services import app, db
 
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///pawesomeDB.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pawesomeDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
